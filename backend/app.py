4d6011c2a3f903b11fbbdfa9e030bde684c2e116
amanmehta276 <166599351+amanmehta276@users.noreply.github.com>
2026-04-06 21:13:51 +0530

    fix render port binding

--- backend/app.py
+++ backend/app.py
@@ -23,6 +23,7 @@ def create_app():
     CORS(app, origins=[
         "http://localhost:3000", "http://127.0.0.1:5500",
         "http://localhost:8000", "http://localhost:5500",
+        "https://cgitsangam.netlify.app/","https://sangam-z93f.onrender.com"
     ], supports_credentials=True)
 
     init_db(app)
